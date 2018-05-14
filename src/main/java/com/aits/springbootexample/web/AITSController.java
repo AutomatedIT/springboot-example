a8ec026ca2e52e8ae3efce9a7b2c15af08342231
John Pikoulas <john.pikoulas@ros.gov.uk>
2018-05-14 15:31:24 +0100

    adding a note through web form

--- src/main/java/com/aits/springbootexample/web/AITSController.java
+++ src/main/java/com/aits/springbootexample/web/AITSController.java
@@ -1,7 +1,9 @@
 package com.aits.springbootexample.web;
 
+import com.aits.springbootexample.Note;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AITSController {
@@ -12,5 +14,17 @@ public class AITSController {
     @RequestMapping(value = "/")
     public String index(){
         return "index";
+    }
+
+
+    @GetMapping("/note")
+    public String noteForm(Model model) {
+        model.addAttribute("note", new Note("note2", "note2 context"));
+        return "note";
+    }
+
+    @PostMapping("/note")
+    public String noteSubmit(@ModelAttribute Note note) {
+        return "result";
     }
 }
